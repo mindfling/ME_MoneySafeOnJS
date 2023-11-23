1da7e02fbae7c58491de0c82d65328b2666483f0
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-11-24 00:56:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
+import { conventStringNumber } from "./convertStringNumber.js";
+console.log('conventStringNumber: ', conventStringNumber('345acbd'));
+
+
 console.log('Hellllo Finance');
+
+
+const report = document.querySelector('.report')
+
+const financeReportBtn = document.querySelector('.finance__report');
+console.log('financeReportBtn: ', financeReportBtn);
+financeReportBtn.addEventListener('click', e => {
+  e.preventDefault();
+  console.log(e.target);
+  report.classList.toggle('report__open');
+});
+
+
+const financeForm = document.querySelector('.finance__form');
+console.log('форма financeForm: ', financeForm);
+
+
+const financeAmount = financeForm.querySelector('.finance__amount');
+console.log('financeAmount: ', financeAmount);
+
+let amount = 22;
+
+financeAmount.textContent = amount;
+
+
+financeForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const target = e.target;
+  const submitter = e.submitter;
+  const typeOperation = submitter?.dataset?.typeOperation;
+  console.log(target, e.submitter, typeOperation);
+});
+
+
+
+// const report = document.querySelector('.report')
+// report.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   const target = e.target;
+  
+// })
+
